6ae47ffae51ef2a8bc67365ba5c67f49b51ac3ad
HP <HP@DESKTOP-LUQBNKH>
2024-02-24 10:50:25 +0530

    Add project files.

---
+++ BankApp/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankApp.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
